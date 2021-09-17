837bc81c6e395027f5885ed6deef56e3f60384c9
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-17 16:23:13 +0530

    Second Commit

--- src/main/java/learn/demo/HelloWorld.java
+++ src/main/java/learn/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package learn.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Bye World!!");
     }
 }
